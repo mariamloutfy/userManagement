d69ce7af365b8eaf5253d279c3e67f08131d9a03
mariamloutfy <maryamloutfy27@gmail.com>
2025-03-27 00:36:53 +0200

    return user count for all departments

--- controllers/departments/list_departments.py
+++ controllers/departments/list_departments.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify
+from flask import jsonify
 import psycopg2
 from config.database import connect_db
 
@@ -6,14 +6,27 @@ def get_departments():
     try:
         conn = connect_db()
         cur = conn.cursor()
-        cur.execute("SELECT id, departmentname, isactive FROM departments")
+        cur.execute("""
+            SELECT d.id, d.departmentname, d.isactive, COUNT(u.id) 
+            FROM departments d 
+            LEFT JOIN users u ON d.id = u.department_id
+            GROUP BY d.id, d.departmentname, d.isactive
+        """)
+        
         departments = cur.fetchall()
         if not departments:
             return jsonify({"message": "No departments found"}), 404
+
         department_list = [
-            {"id": department[0], "depname": department[1], "isActive": department[2]}
+            {
+                "id": department[0], 
+                "depname": department[1], 
+                "isActive": department[2], 
+                "userCount": department[3]  # Adding user count
+            }
             for department in departments
         ]
+
         return jsonify(department_list), 200
     except psycopg2.Error as e:
         return jsonify({"error": "Database error", "details": str(e)}), 500
