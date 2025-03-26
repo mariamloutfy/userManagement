9a204f759827a0ac40bd6e1fe110083cbefe0ad4
mariamloutfy <maryamloutfy27@gmail.com>
2025-03-27 00:42:03 +0200

    return department name for listing users

--- controllers/users/list_users.py
+++ controllers/users/list_users.py
@@ -1,19 +1,32 @@
-from flask import request, jsonify
+from flask import jsonify
 import psycopg2
 from config.database import connect_db
-from utils import is_valid_email
+
 def get_users():
     try:
         conn = connect_db()
         cur = conn.cursor()
-        cur.execute("SELECT id, username, email, phone FROM users")
+        cur.execute("""
+            SELECT u.id, u.username, u.email, u.phone, d.departmentname 
+            FROM users u
+            LEFT JOIN departments d ON u.department_id = d.id
+        """)
         users = cur.fetchall()
+        
         if not users:
             return jsonify({"message": "No users found"}), 404
+
         user_list = [
-            {"id": user[0], "username": user[1], "email": user[2], "phone": user[3]}
+            {
+                "id": user[0], 
+                "username": user[1], 
+                "email": user[2], 
+                "phone": user[3], 
+                "department": user[4] if user[4] else "No Department"
+            }
             for user in users
         ]
+
         return jsonify(user_list), 200
     except psycopg2.Error as e:
         return jsonify({"error": "Database error", "details": str(e)}), 500
