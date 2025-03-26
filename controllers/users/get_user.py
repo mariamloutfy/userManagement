2db4e4296ceb294a83153b343d2ae4a11404270b
mariamloutfy <maryamloutfy27@gmail.com>
2025-03-27 00:41:05 +0200

    return department name for get user

--- controllers/users/get_user.py
+++ controllers/users/get_user.py
@@ -2,11 +2,18 @@ from flask import request, jsonify
 import psycopg2
 from config.database import connect_db
 from utils import is_valid_email
+
 def get_user(user_id):
     try:
         conn = connect_db()
         cur = conn.cursor()
-        cur.execute("SELECT id, username, email, phone,department_id  FROM users WHERE id = %s", (user_id,))
+        cur.execute("""
+            SELECT u.id, u.username, u.email, u.phone, d.departmentname 
+            FROM users u 
+            LEFT JOIN departments d ON u.department_id = d.id
+            WHERE u.id = %s
+        """, (user_id,))
+        
         user = cur.fetchone()
         if not user:
             return jsonify({"error": "User not found"}), 404
@@ -15,8 +22,9 @@ def get_user(user_id):
             "username": user[1],
             "email": user[2],
             "phone": user[3],
-            "department_id": user[4]
+            "department": user[4]  # Returns department name instead of ID
         }
+
         return jsonify(user_data), 200
     except psycopg2.Error as e:
         return jsonify({"error": "Database error", "details": str(e)}), 500
